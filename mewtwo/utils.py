7c3e199369a40c1b39a7a81c6d952295cb23db2c
chalice <chalice@unnotech.com>
2019-03-30 11:11:27 +0800

    fix

--- mewtwo/utils.py
+++ mewtwo/utils.py
@@ -4,10 +4,11 @@ from uuid import uuid4
 from django.utils.translation import ugettext as _
 from django_otp.plugins.otp_totp.models import TOTPDevice
 from rest_framework import renderers
-from rest_framework.exceptions import ErrorDetail
 from oauth2_provider.models import AccessToken
 from mewtwo.lib import constants
 from django.utils.deconstruct import deconstructible
+from rest_framework.views import exception_handler
+from rest_framework.exceptions import ErrorDetail, Throttled
 
 
 logger = logging.getLogger(__name__)
@@ -242,3 +243,51 @@ class PathAndRename(object):
 
         # return the whole path to the file
         return os.path.join(self.path, filename)
+
+
+def custom_exception_handler(exc, context):
+    """
+    Call REST framework's default exception handler first,
+    to get the standard error response.
+
+    This overriden handler should standardize the error response
+    format for ghost:
+    response.data = {key: [list of errors]}
+    """
+
+    response = exception_handler(exc, context)
+    request = context.get('request', None)
+    if isinstance(exc, Throttled):
+        throttled_data = {'error': [f'支付请求太频繁，请{exc.wait}s后再来']}
+        response.data = throttled_data
+        return response
+    if request and request.META.get('HTTP_AUTHORIZATION'):
+        try:
+            token = request.META.get('HTTP_AUTHORIZATION').split(' ')
+            access_token = token[1]
+            token_obj = AccessToken.objects.get(token=access_token)
+            if token_obj.is_expired():
+                response.data['error'] = 'Token has already expired'
+        except Exception as exc:
+            logger.error(exc)
+            pass
+    try:
+        if response.status_code in [400, 401, 404, 403, 405, 429]:
+            data_cpy = response.data.copy()
+            error_list = []
+            if isinstance(data_cpy, dict):
+                for key, msg in data_cpy.items():
+                    error_msg = msg
+                    if isinstance(key, int) and isinstance(msg, list):
+                        error_msg = msg[0]
+                    if key == 'error' or key == 'detail':
+                        error_list.append(error_msg)
+                        continue
+                    error_list.append({key: error_msg})
+            elif isinstance(data_cpy, list):
+                error_list = response.data
+            response.data = {'error': error_list}
+    except Exception as exc:
+        logger.error(exc)
+
+    return response
